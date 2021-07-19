27bee28c8c1462fe80d612a8531622186d876c2e
Nick Lee <raven1300@hotmail.com>
2021-07-19 22:00:59 +1000

    added comment to javascript file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,6 +3,7 @@
 const burgerIcon = document.querySelector("#burger");
 const navbarMenu = document.querySelector("#nav-links");
 
+// show and hide the menu in mobile mode by clicking the buger button
 burgerIcon.addEventListener("click", () => {
     navbarMenu.classList.toggle("is-active");
 });
